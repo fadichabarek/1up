3bf441d1adbd223728d0103e234fd9756ffbb84d
fchabarek <fadi.chabarek@web.de>
2016-05-22 19:50:05 +0200

    add ra as root package

--- 1up-ra/src/main/java/one/up/ra/Application.java
+++ 1up-ra/src/main/java/one/up/ra/Application.java
@@ -1,4 +1,4 @@
-package one.up;
+package one.up.ra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- 1up-ra/src/test/java/one/up/ra/ApplicationTests.java
+++ 1up-ra/src/test/java/one/up/ra/ApplicationTests.java
@@ -1,5 +1,6 @@
-package one.up;
+package one.up.ra;
 
+import one.up.ra.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
